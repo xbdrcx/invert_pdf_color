d5b00456a09c8a4ab08ff7a50225cef3746165bb
Bruno Cruz <cruzzz.1998@live.com.pt>
2023-08-07 19:08:31 +0100

    Commit
    Added Poppler. Added error handling.

--- invert.py
+++ invert.py
@@ -1,18 +1,32 @@
-# pip install pdf2img
-# pip install img2pdf
+# This version uses Poppler V23.07.0 (https://github.com/oschwartz10612/poppler-windows/releases/)
+# When it executes for the first time it creates a VARIABLE PATH on the computer pointing to the existing Poppler folder in the project directory
+# - pip install pdf2img
+# - pip install img2pdf
+from tkinter.filedialog import askopenfilename
 from pdf2image import convert_from_path
+from pdf2image.exceptions import (
+        PDFInfoNotInstalledError,
+        PDFPageCountError,
+        PDFSyntaxError
+)
 from PIL import ImageOps
-import os, img2pdf
+import os, sys, img2pdf
+
+POPPLER_PATH = os.path.dirname(os.path.abspath(__file__)) + r"\poppler-23.07.0\library\bin"
+
+sys.path.append(POPPLER_PATH)
 
 def InvertPdfColor(filepath):
 
     if os.path.exists("output/") == False:
         os.mkdir("output/")
 
-    file_basename = os.path.basename(filepath)
-    file_name = os.path.splitext(file_basename)[0]
-
-    images = convert_from_path(filepath)
+    file_name = os.path.splitext(os.path.basename(filepath))[0]
+    
+    try:
+        images = convert_from_path(filepath, poppler_path=POPPLER_PATH)
+    except (PDFInfoNotInstalledError, PDFPageCountError, PDFSyntaxError) as err:
+        print(err)
 
     idx_counter = []
     for idx, i in enumerate(images):
@@ -26,14 +40,17 @@ def InvertPdfColor(filepath):
     for i in idx_counter:
         os.remove(i)
 
-
 if __name__ == "__main__":
-    print("Invert PDF Color 1.0")
+    print("Invert PDF Color")
     print("Welcome\n")
-    path = str(input("File path: "))
+    print("Opening file dialog...")
+    path = askopenfilename(title="Choose PDF to Invert Colors", initialdir=POPPLER_PATH, filetypes=[("PDF", "*.pdf")])
+    print("Choosen PDF: ", path)
     try:
+        print("Inverting PDF colors...")
         InvertPdfColor(path)
         print("Finished.")
     except:
-        print("Error occured while trying to invert pdf color.")
+        print("Error occured while trying to invert PDF colors.")
     exit(0)
+    
